633e8e45ec158542df4bf24e2d9975fd729bd4fd
Isaac Beh <isaacwbeh@gmail.com>
2024-08-16 20:56:54 +1000

    Added graph visualisation demo

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from pyvis.network import Network
+import networkx as nx
 import chromadb
+
 chroma_client = chromadb.Client()
 
 collection = chroma_client.create_collection(name="flashcards")
@@ -17,3 +20,17 @@ results = collection.query(
     n_results=2 # how many results to return
 )
 print(f"Results {results}")
+
+nx_graph = nx.cycle_graph(10)
+nx_graph.nodes[1]['title'] = 'Number 1'
+nx_graph.nodes[1]['group'] = 1
+nx_graph.nodes[3]['title'] = 'I belong to a different group!'
+nx_graph.nodes[3]['group'] = 10
+nx_graph.add_node(20, size=20, title='couple', group=2)
+nx_graph.add_node(21, size=15, title='couple', group=2)
+nx_graph.add_edge(20, 21, weight=5)
+nx_graph.add_node(25, size=25, label='lonely', title='lonely node', group=3)
+nt = Network('500px', '500px')
+# populates the nodes and edges data structures
+nt.from_nx(nx_graph)
+nt.save_graph("demo.html")
