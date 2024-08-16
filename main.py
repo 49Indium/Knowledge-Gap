51957d848934c7b5fe30515293a6d53819aab91e
Isaac Beh <isaacwbeh@gmail.com>
2024-08-16 21:32:58 +1000

    Added reading from mathematical definition notes

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
 from pyvis.network import Network
 import networkx as nx
 import chromadb
+import csv
 
-chroma_client = chromadb.Client()
+def read_mathematical_definition_notes(filepath="data/MathsDefinitionNotes.txt"):
+    with open(filepath, newline="") as notefile:
+        note_reader = csv.reader(notefile, delimiter="\t")
+        # Skip over headings
+        for _ in range(6):
+            next(note_reader)
+            
+        notes:list[dict[str, str]] = []
+            
+        for row in note_reader:
+            id, note_type, deck, term, symbol, definition, extra_notes, tags = row
+            assert (note_type == "08 Mathematical Definition")
 
-collection = chroma_client.create_collection(name="flashcards")
+            # Remove custom LaTeX tags
+            def replace_latex(s):
+                return s.replace("[$]","$").replace("[/$]","$").replace("[$$]","$$").replace("[/$$]","$$")
+            
+            term = replace_latex(term)
+            symbol = replace_latex(symbol) if symbol else None
+            definition = replace_latex(definition)
 
-collection.add(
-    documents=[
-        "This is a document about pineapple",
-        "This is a document about oranges"
-    ],
-    ids=["id1", "id2"]
-)
+            notes.append({
+                             "id": id,
+                             "note_type": note_type,
+                             "deck": deck,
+                             "term": term,
+                             "definition": definition,
+                             "extra_notes": extra_notes,
+                             "tags": tags,
+                         } | ({"symbol": symbol} if symbol else {}))
 
-print("hi")
-results = collection.query(
-    query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
-    n_results=2 # how many results to return
+        return notes
+
+def add_mathematical_definition_notes_to_db(db, notes):
+    db.add(
+        documents = [f"{note['term']} := {note['definition']}" for note in notes],
+        metadatas = notes,
+        ids = [note["id"] for note in notes]
+    )
+            
+chroma_client = chromadb.PersistentClient(path="/data/chromadb")
+
+flashcards_db = chroma_client.get_or_create_collection(name="flashcards")
+# chroma_client.delete_collection(name="flashcards")
+
+# notes = read_mathematical_definition_notes()
+# add_mathematical_definition_notes_to_db(flashcards_db, notes)
+
+# print(flashcards_db.peek(1))
+
+results = flashcards_db.query(
+    query_texts=["clopen"],
+    n_results=5
 )
 print(f"Results {results}")
 
